776841a2340b6232854597c2f97c04ed200c4690
BabyPeach <kelvinhoh1520@hotmail.com>
2020-06-07 21:31:37 +0800

    Working on Main Interface

--- App.js
+++ App.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.lists}>
+        <View style={styles.list}>
+          <Text style={styles.listButton}>O</Text>
+          <TouchableOpacity style={styles.listTitle}>
+            <Text style={{ marginLeft: 10 }}>Cook Something</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.list}>
+          <Text style={styles.listButton}>O</Text>
+          <TouchableOpacity style={styles.listTitle}>
+            <Text style={{ marginLeft: 10 }}>Cook Something</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity activeOpacity={.6} style={[styles.button, styles.buttonActive]}>
+          <Text>Normal</Text>
+        </TouchableOpacity>
+        <TouchableOpacity activeOpacity={.6} style={styles.button}>
+          <Text>Important</Text>
+        </TouchableOpacity>
+        {/* <Button style={styles.button} title="Normal" />
+        <Button style={styles.button} title="Important" /> */}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  screen: {
+    marginTop: 45,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
   },
+  lists: {
+    height: '92%',
+  },
+  list: {
+    flexDirection: 'row',
+    fontSize: 12,
+    height: '5%',
+    marginVertical: 15,
+  },
+  listButton: {
+    marginHorizontal: 25,
+  },
+  listTitle: {
+    flex: 1,
+    borderBottomWidth: 2,
+    borderBottomColor: '#BDBDBD',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flex: 1,
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#EBEBEB',
+  },
+  buttonActive: {
+    backgroundColor: '#DADADA',
+  }
 });
